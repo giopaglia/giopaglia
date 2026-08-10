40e9cdb4639f7e95b60dc8ea550968c46d3e6a30
giopaglia <24519853+giopaglia@users.noreply.github.com>
2026-08-10 18:13:52 +0200

    Fix

--- README.py
+++ README.py
@@ -139,9 +139,9 @@ I'm Gio (/d͡ʒo/), a computer scientist based in Ferrara, Italy.
     <td align="center" width="95">
       <a href="https://www.sublimetext.com/" title="Scratchpads and huge files">
         <img src="https://cdn.simpleicons.org/sublimetext" width="42" height="42" alt="Sublime Text"><br>
-      </a>&nbsp;&nbsp;<a href="https://www.sublimetext.com/" title="Scratchpads and huge files">
-        <b>Sublime</b>
-      </a>&nbsp;&nbsp;
+      </a><a href="https://www.sublimetext.com/" title="Scratchpads and huge files">
+        &nbsp;&nbsp;<b>Sublime</b>&nbsp;&nbsp;
+      </a>
     </td>
     <td align="center" width="95">
       <a href="https://github.com/jesseduffield/lazygit" title="Handy git client">
@@ -199,9 +199,9 @@ I'm Gio (/d͡ʒo/), a computer scientist based in Ferrara, Italy.
     <td align="center" width="95">
       <a href="https://lmstudio.ai/" title="LLM workflows">
         <img src="https://cdn.simpleicons.org/lmstudio/000000/ffffff" width="42" height="42" alt="LM Studio"><br>
-      </a>&nbsp;<a href="https://lmstudio.ai/" title="LLM workflows">
-        <b>LM&nbsp;Studio</b>
-      </a>&nbsp;
+      </a><a href="https://lmstudio.ai/" title="LLM workflows">
+        &nbsp;<b>LM&nbsp;Studio</b>&nbsp;
+      </a>
     </td>
     <td align="center" width="95">
       <a href="https://ollama.com/" title="LLM workflows, when LM Studio doesn't cut it">
